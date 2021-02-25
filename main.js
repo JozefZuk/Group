a3db31c8702a46c14ca80e86abd82ec50de2edd7
JozefZuk <51132472+JozefZuk@users.noreply.github.com>
2021-02-25 22:00:00 +0100

    creating script for creating new cart with name

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 const addBtn = document.querySelector("#addBtn");
+const mainInput = document.querySelector("#mainInput");
+const readyNamesBox = document.querySelector("#readyNamesBox");
+
 let allName = [];
+let i;
+let nameTags = [];
+let btnId = [];
+
 
 addBtn.addEventListener("click", function(){
 
     var nameValue = document.querySelector("#mainInput").value;
 
-    allName += nameValue;
+    if(nameValue == 0){
+        alert("You didn't write anything")
+    }
+
+        allName[i] = nameValue;
+
+        nameTags[i] = document.createElement("DIV");
+        readyNamesBox.appendChild(nameTags[i]);
+        nameTags[i].classList.add("nameTags");
+        nameTags[i].innerHTML = allName[i]
+        
+        
 
 })
