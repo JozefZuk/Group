74ef57d92dd4fe414e2a5dca63646770b37b63af
JozefZuk <51132472+JozefZuk@users.noreply.github.com>
2021-03-19 10:41:30 +0100

    adding buttons

--- main.js
+++ main.js
@@ -2,33 +2,48 @@ const addBtn = document.querySelector("#addBtn");
 const mainInput = document.querySelector("#mainInput");
 const readyNamesBox = document.querySelector("#readyNamesBox");
 
-let allName = [];
-let readyCart = [];
+let cart = [];
 let i = 0;
 
-class nameCart {
-    constructor(name, id) {
+class cartValue {
+    constructor(name, id, btn, box) {
       this.name = name;
       this.id = id;
+      this.btn = btn;
+      this.box = box;
     };
   };
 
 addBtn.addEventListener("click", function(){
-    allName[i] = new  nameCart();
-    allName[i].name = document.querySelector("#mainInput").value;
-    allName[i].id = i;
+    cart[i] = new  cartValue();
+    cart[i].name = document.querySelector("#mainInput").value;
+    cart[i].id = i;
 
-    if(allName[i].name == 0){
+    if(cart[i].name == 0){
         alert("You didn't write anything");
     } 
 
     else{   
-        readyCart[i] = document.createElement("DIV");
-        readyCart[i].innerHTML = allName[i].name;
-        readyNamesBox.appendChild(readyCart[i]);
+        cart[i].box = document.createElement("DIV");
+        cart[i].btn = document.createElement("BUTTON");
 
+        cart[i].box.innerHTML = cart[i].name;
+        cart[i].btn.innerHTML = "Delete";
+        cart[i].btn.value = i;
+
+        readyNamesBox.appendChild(cart[i].box);
+        cart[i].box.appendChild(cart[i].btn);
+
+        cart[i].btn.addEventListener("click", function(){
+            cart[this.value].box.innerHTML = '';
+            cart[this.value].name = 0;
+            i --;
+            alert(this.value);
+
+        })
+        
+    
         i ++;
-
-}
+        mainInput.value = "";
+    }
 });        
-
